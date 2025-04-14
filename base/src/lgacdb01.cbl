7f9346e85d355b23938cb95e80232b3eecc70c7b
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2025-04-14 11:27:15 +0200

    ERROR

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -10,7 +10,7 @@
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. LGACDB0.
+       PROGRAM-ID. LGACDB.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
       *
