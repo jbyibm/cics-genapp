284236103baf32d61c1d9e02543327db74da6223
Pratima <pratima@ibm.com>
2025-12-05 10:27:50 +0530

    Demo

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -10,7 +10,7 @@
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID.     LGAD01. 
+       PROGRAM-ID.     LGA01. 
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
       *
@@ -283,11 +283,12 @@
                EXEC SQL
                  SET :DB2-CUSTOMERNUM-INT = IDENTITY_VAL_LOCAL()
                END-EXEC
-               IF SQLCODE NOT EQUAL 0
+                IF SQLCODE NOT EQUAL 0
                MOVE '90' TO CA-RETURN-CODE
                PERFORM WRITE-ERROR-MESSAGE
                EXEC CICS RETURN END-EXEC
              END-IF
+               
            END-IF.
 
            MOVE DB2-CUSTOMERNUM-INT TO CA-CUSTOMER-NUM.
