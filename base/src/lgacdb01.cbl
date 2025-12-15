0ba7cb0c4ed8ace566a5edbb8ad237362ab7d5ed
Pratima <pratima@ibm.com>
2025-12-15 11:23:58 +0530

    Test

--- base/src/lgacdb01.cbl
+++ base/src/lgacdb01.cbl
@@ -10,7 +10,7 @@
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID.     LDB01. 
+       PROGRAM-ID.     LGACDB01. 
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
       *
